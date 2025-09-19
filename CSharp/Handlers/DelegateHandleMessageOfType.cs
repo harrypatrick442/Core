0d3305eb0042fe7c7c9e83fe73227b1bed4f503e
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2025-09-19 15:51:01 +0100

    Moved.

---
+++ CSharp/Handlers/DelegateHandleMessageOfType.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+using Core.Events;
+using Core.Interfaces;
+using Core.Messages;
+using Snippets.WebAPI.WebsocketServers;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Handlers
+{
+
+    public delegate void DelegateHandleMessageOfType<TPayload>(TPayload payload);
+}
---
+++ CSharp/Processes/RunningProcessHandle.cs
@@ -0,0 +1,43 @@
+﻿using Core.Events;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Core.Processes
+{
+    public class RunningProcessHandle : IDisposable
+    {
+        private Action _Dispose;
+        private Action<int?> _Wait;
+        private bool _Disposed = false;
+        private Process _Process;
+        public RunningProcessHandle(Action dispose, Action<int?> wait, Process process)
+        {
+            _Dispose = dispose;
+            _Wait = wait;
+            _Process = process;
+        }
+        public void WriteLine(string str) {
+            _Process.StandardInput.WriteLine(str);
+        }
+        public int Wait(int? millisecondsTimeout = null)
+        {
+            _Wait(millisecondsTimeout);
+            return _Process.ExitCode;
+        }
+
+        ~RunningProcessHandle() {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            lock (_Dispose)
+            {
+                if (_Disposed) return;
+                _Disposed = true;
+                _Dispose();
+            }
+        }
+    }
+}
